22aec1c30dcae5ffe2fc46cf8a69c099cc5bcbb4
Joaquina Daeron <joaquind@ge-k6.kzn.21-school.ru>
2022-08-20 14:51:46 +0300

    Движение ракеток готово

--- src/pong.c
+++ src/pong.c
@@ -10,65 +10,61 @@ int locate_ball(char ch);
 void print_field(int pos_1, int pos_2);
 
 int main(void) {
-    int i, sc_1 = 0, sc_2 = 0;
+    int count, sc_1 = 0, sc_2 = 0;
     int rocket_1 = 12, rocket_2 = 12;
     char command;
     while (sc_1 < 21 && sc_2 < 21) {
         scanf("%c", &command);
-        getchar();
-        rocket_1 = locate_rocket_1 (rocket_1, command);
+        while (getchar() != '\n');
+        rocket_1 = locate_rocket_1(rocket_1, command);
+        printf("rocet_1 = %d\n", rocket_1);
+        rocket_2 = locate_rocket_2(rocket_2, command);
         print_field(rocket_1, rocket_2);
-        /*for (i = 1; i <= LENGHT; i++)
-            printf("-");
-        printf("\n");
-        for (i = 1; i <= HEIGHT; i++){
-            printf("|");
-            if ((i >= rocket_1) && (i < rocket_1 + PLATFORM))
-                printf("#");
-            if ((i >= rocket_2) && (i < rocket_2 + PLATFORM))
-                printf("%*c|\n", LENGHT - 3, '#');
-            else
-                printf("%*c\n", LENGHT - 1, '|');
-        }
-        for (i = 1; i <= LENGHT; i++)
-            printf("-");
-        printf("\n");*/
     }
         return 0;
 }
 
 void print_field(int pos_1, int pos_2) {                // Печатает поле
-    int i, flag_1 = 1;
-    for (i = 1; i <= LENGHT; i++)
+    int count, flag_1 = 1;
+    for (count = 1; count <= LENGHT; count++)
         printf("-");
     printf("\n");
-    for (i = 1; i <= HEIGHT; i++){
+    for (count = 1; count <= HEIGHT; count++){
         printf("|");
-        if ((i >= pos_1) && (i < pos_1 + PLATFORM)) {
+        if ((count >= pos_1) && (count < pos_1 + PLATFORM)) {
             printf("#");
-            flag_1 = 0;
-        } else {
             flag_1 = 1;
-        }
-        if ((i >= pos_2) && (i < pos_2 + PLATFORM)) {
-            printf("%*c", LENGHT - 3  - flag_1, '#');
-            printf("|\n");
         } else {
-            printf("%*c\n", LENGHT - flag_1, '|');
+            flag_1 = 0;
+        }
+        if ((count >= pos_2) && (count < pos_2 + PLATFORM)) {
+            printf("%*c|", LENGHT - 2 - flag_1, '#');
+            printf("\n");
+        } else {
+            printf("%*c\n", LENGHT - 1 - flag_1, '|');
             
         }
             
     }
-    for (i = 1; i <= LENGHT; i++)
+    for (count = 1; count <= LENGHT; count++)
         printf("-");
     printf("\n");
 }
 
 int locate_rocket_1(int pos, char ch) {             // Определяет позицию левой ракетки после ввода команды
-    if (ch == 'a')
-        return pos++;
-    else if (ch == 'z')
-        return pos--;
-    else
-        while (getchar() != '\n');
+    int res = pos;
+    if (ch == 'z') {
+        return ++res;
+    } else if (ch == 'a') {
+        return --res;
+    }
+}
+
+int locate_rocket_2(int pos, char ch) {             // Определяет позицию левой ракетки после ввода команды
+    int res = pos;
+    if (ch == 'm') {
+        return ++res;
+    } else if (ch == 'k') {
+        return --res;
+    }
 }
